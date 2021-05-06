679bf977756e5a6dcca3c27312c3433446a62f20
Vítor Vieira <jose.vieira153@gmail.com>
2021-05-06 17:14:08 +0100

    [FIX] adds many improvements to ipfba models
    - matrices are deleted and garbage collected earlier
    - igpr models now save their parent metabolic model
    reactions and compounds
    - module reformat

--- src/imgrasp/integration/models.py
+++ src/imgrasp/integration/models.py
@@ -1,22 +1,22 @@
+import gc
+from collections import OrderedDict
 from itertools import chain
 
-from collections import OrderedDict
+import numpy as np
 
 from cobamp.core.models import ConstraintBasedModel
 from cobamp.core.optimization import Solution
 from cobamp.wrappers import get_model_reader
-
-import gc
-import numpy as np
-
 from imgrasp.graph.core import GenericGraph
 
+
 class IntegratedGPRSolution(Solution):
-    def  __init__(self, full_solution: Solution, reaction_mapping, gene_expression_prefix, **kwargs):
+    def __init__(self, full_solution: Solution, reaction_mapping, gene_expression_prefix, **kwargs):
         ov = full_solution.objective_value()
         names, values = zip(*full_solution.var_values().items())
+        values = list(values)
         metabolic_solution = OrderedDict()
-        for k,v in reaction_mapping.items():
+        for k, v in reaction_mapping.items():
             if not isinstance(v, int):
                 flux = values[v[0]] - values[v[1]]
                 if flux >= 0:
@@ -31,17 +31,16 @@ class IntegratedGPRSolution(Solution):
             metabolic_solution[names[k]] = flux
 
         gene_activity_solution = OrderedDict()
-        for i,k in enumerate(names):
+        for i, k in enumerate(names):
             if k.startswith(gene_expression_prefix):
                 gene_activity_solution[k[len(gene_expression_prefix):]] = values[i]
 
         self.__gene_activity_solution = gene_activity_solution
         kwargs['names'] = list(metabolic_solution.keys())
-        super().__init__(value_map={i:metabolic_solution[k] for i,k in enumerate(metabolic_solution.keys())},
+        super().__init__(value_map={i: metabolic_solution[k] for i, k in enumerate(metabolic_solution.keys())},
                          status=full_solution.status(), objective_value=ov, **kwargs)
 
-
-        self.all_reactions = {k:full_solution.var_values()[k] for k in set(names)}
+        self.all_reactions = {k: full_solution.var_values()[k] for k in set(names)}
 
     @property
     def gene_activity(self):
@@ -50,15 +49,16 @@ class IntegratedGPRSolution(Solution):
 
 def get_linear_gpr_model(model_object, max_flux=1e4, gene_drain_prefix='GXD_', gene_metab_prefix='GXM_',
                          reaction_metab_prefix='RXM_', reaction_conv_prefix='RXD_', gpr_component_metab_prefix='GCOMP_',
-                         and_component_prefix='COMP_', or_component_prefix='ORCOMP_', initialize_optimizer=False):
-
+                         and_component_prefix='COMP_', or_component_prefix='ORCOMP_', initialize_optimizer=False,
+                         return_gpr_map=False):
     model = get_model_reader(model_object)
     gpr_container = model.gene_protein_reaction_rules
     gene_names = gpr_container.get_genes()
     gene_names_map = dict(zip(gene_names, range(len(gene_names))))
     added_nodes = []
+
     rx_to_node = {}
-    for r_id, i in zip(model.r_ids,range(len(gpr_container))):
+    for r_id, i in zip(model.r_ids, range(len(gpr_container))):
         x = [frozenset([k for k in isoform if k in gene_names])
              for isoform in gpr_container.get_gpr_as_lists(i)]
         if len(x) > 0 and len(x[0]) > 0:
@@ -66,38 +66,37 @@ def get_linear_gpr_model(model_object, max_flux=1e4, gene_drain_prefix='GXD_', g
                 if node_set not in added_nodes:
                     added_nodes.append(node_set)
             rx_to_node[r_id] = x
-    gene_node_dict = {i:frozenset([x]) for i, x in enumerate(gene_names)}
-    intermediate_node_dict = {i:k for i, k in
+    gene_node_dict = {i: frozenset([x]) for i, x in enumerate(gene_names)}
+    intermediate_node_dict = {i: k for i, k in
                               enumerate(set(added_nodes) - set(gene_node_dict.values()))}
 
-    node_union = {v:k for k,v in gene_node_dict.items()}
-    node_union.update({v:k+len(gene_node_dict) for k,v in intermediate_node_dict.items()})
-    reaction_dict = {r_id[0]:i  for i, r_id in enumerate(rx_to_node.items())}
-
+    node_union = {v: k for k, v in gene_node_dict.items()}
+    node_union.update({v: k + len(gene_node_dict) for k, v in intermediate_node_dict.items()})
+    reaction_dict = {r_id[0]: i for i, r_id in enumerate(rx_to_node.items())}
 
     # G - gene by composite_node matrix (m genes by n extra_nodes)
     G = np.zeros([len(gene_node_dict), len(intermediate_node_dict)])
     int_nodes_list = list(intermediate_node_dict.values())
-    for i,k in enumerate(int_nodes_list):
+    for i, k in enumerate(int_nodes_list):
         g_idx = [gene_names_map[x] for x in k]
-        G[g_idx,i] = -1
+        G[g_idx, i] = -1
 
     # R - gene/intermediate on rows vs individual OR components for reactions
     # Q - identity: rows = reaction metabolites vs columns = OR components
 
     comp_rows, pseudo_identity = [], []
-    row_dim = sum(map(len,[gene_node_dict,intermediate_node_dict]))
+    row_dim = sum(map(len, [gene_node_dict, intermediate_node_dict]))
     for rx, nodes in rx_to_node.items():
-        rows = np.zeros([row_dim,len(nodes)])
-        for i,n in enumerate(nodes):
-            rows[node_union[n],i] = -1
+        rows = np.zeros([row_dim, len(nodes)])
+        for i, n in enumerate(nodes):
+            rows[node_union[n], i] = -1
         comp_rows.append(rows)
-        ident_row = np.zeros([len(reaction_dict),len(nodes)])
-        ident_row[reaction_dict[rx],:] = 1
+        ident_row = np.zeros([len(reaction_dict), len(nodes)])
+        ident_row[reaction_dict[rx], :] = 1
         pseudo_identity.append(ident_row)
 
     R, Q = (np.concatenate(x, axis=1) for x in [comp_rows, pseudo_identity])
-    Rg, Rc = R[:len(gene_names),:], R[len(gene_names):,:]
+    Rg, Rc = R[:len(gene_names), :], R[len(gene_names):, :]
     gn, cn, orcn, rxn = len(gene_names), len(intermediate_node_dict), R.shape[1], Q.shape[0]
 
     del R
@@ -105,37 +104,49 @@ def get_linear_gpr_model(model_object, max_flux=1e4, gene_drain_prefix='GXD_', g
     mat_blocks = [
         [np.eye(gn), G, Rg, np.zeros([gn, rxn])],
         [np.zeros([cn, gn]), np.eye(cn), Rc, np.zeros([cn, rxn])],
-        [np.zeros([rxn, gn+cn]), Q, -np.eye(rxn)]
+        [np.zeros([rxn, gn + cn]), Q, -np.eye(rxn)]
     ]
 
-    rxd_ordered = {v:k for k,v in reaction_dict.items()}
+    rxd_ordered = {v: k for k, v in reaction_dict.items()}
     rxd_list_ordered = [rxd_ordered[i] for i in range(len(reaction_dict))]
     full_mat = np.vstack(list(map(np.hstack, mat_blocks)))
 
     del mat_blocks, Rg, Rc, Q
 
-    rx_identifiers = [gene_drain_prefix+g for g in gene_names] + [and_component_prefix+str(i) for i in range(len(int_nodes_list))] + \
-                     list(chain(*[[''.join([or_component_prefix,rx,'_'+str(i)]) for i in range(len(rx_to_node[rx]))]
-                                  for rx in rxd_list_ordered])) + \
-                     [reaction_conv_prefix+rx for rx in rxd_list_ordered]
-    met_identifiers = [gene_metab_prefix+g for g in gene_names] + [gpr_component_metab_prefix+str(i) for i in range(len(int_nodes_list))] + \
-                      [reaction_metab_prefix+r for r in rxd_list_ordered]
+    rx_g_drains = [gene_drain_prefix + g for g in gene_names]
+    rx_and_comp = [and_component_prefix + str(i) for i in range(len(int_nodes_list))]
+    ordered_or_comps = [[''.join([or_component_prefix, rx, '_' + str(i)]) for i in range(len(rx_to_node[rx]))]
+                        for rx in rxd_list_ordered]
+
+    rx_or_comp = list(chain(*ordered_or_comps))
+    rx_or_comp_dict = dict(zip(rxd_list_ordered, ordered_or_comps))
+    rx_conv_id = [reaction_conv_prefix + rx for rx in rxd_list_ordered]
+
+    rx_identifiers = rx_g_drains + rx_and_comp + rx_or_comp + rx_conv_id
+
+    met_g_exp = [gene_metab_prefix + g for g in gene_names]
+    met_or_comp = [gpr_component_metab_prefix + str(i) for i in range(len(int_nodes_list))]
+    met_rx_ids = [reaction_metab_prefix + r for r in rxd_list_ordered]
+
+    rx_to_node_out = {k: ([list(i) for i in v], rx_or_comp_dict[k]) for k, v in rx_to_node.items()}
+    met_identifiers = met_g_exp + met_or_comp + met_rx_ids
 
     gene_model = ConstraintBasedModel(S=full_mat,
-                                      thermodynamic_constraints = [[0,max_flux] for i in range(full_mat.shape[1])],
+                                      thermodynamic_constraints=[[0, max_flux] for i in range(full_mat.shape[1])],
                                       reaction_names=rx_identifiers, metabolite_names=met_identifiers,
                                       optimizer=initialize_optimizer)
 
-    return gene_model, full_mat
-
+    return (gene_model, full_mat, rx_to_node_out) if return_gpr_map else (gene_model, full_mat)
 
 
 def get_integrated_gpr_model(model: ConstraintBasedModel, max_flux=1e4, reaction_metab_prefix='RXM_',
                              reaction_conv_prefix='RXD_'):
     model.model = None
     integrated_irrev_model, cb_model_rev_map = model.make_irreversible()
-    M,N = [len(x) for x in [integrated_irrev_model.metabolite_names, integrated_irrev_model.reaction_names]]
-    gpr_model, _ = get_linear_gpr_model(model, max_flux=max_flux)
+    orig_metabolites, orig_metabolic_fluxes = list(integrated_irrev_model.metabolite_names), \
+                                              list(integrated_irrev_model.reaction_names)
+    M, N = [len(x) for x in [integrated_irrev_model.metabolite_names, integrated_irrev_model.reaction_names]]
+    gpr_model, _, rx_to_gpr = get_linear_gpr_model(model, max_flux=max_flux, return_gpr_map=True)
     rx_to_add = [n for n in gpr_model.reaction_names if n.startswith(reaction_conv_prefix)]
     mt_to_add = [n for n in gpr_model.metabolite_names if n.startswith(reaction_metab_prefix)]
 
@@ -147,15 +158,13 @@ def get_integrated_gpr_model(model: ConstraintBasedModel, max_flux=1e4, reaction
     integrated_irrev_model.add_metabolites(np.zeros([n_gpr_met, n_irrcb_rx]), gpr_model.metabolite_names)
     integrated_irrev_model.set_stoichiometric_matrix(mat_ident, rows=mt_to_add, update_only_nonzero=True)
 
-    print('get_integrated_gpr_model - Adding metabolites...')
     del mat_ident
-
 
     padding = np.zeros([M, len(gpr_model.reaction_names)])
     int_model_mat = np.vstack([padding, gpr_model.get_stoichiometric_matrix()]).astype(int)
 
     del padding
-    iim_bounds, iim_names  = gpr_model.bounds, gpr_model.reaction_names
+    iim_bounds, iim_names = gpr_model.bounds, gpr_model.reaction_names
 
     del gpr_model
 
@@ -167,21 +176,19 @@ def get_integrated_gpr_model(model: ConstraintBasedModel, max_flux=1e4, reaction
 
     print('get_integrated_gpr_model - Adding reactions...')
 
-
-    return integrated_irrev_model, cb_model_rev_map, mt_to_add
+    return integrated_irrev_model, cb_model_rev_map, mt_to_add, rx_to_gpr, orig_metabolic_fluxes, orig_metabolites
 
 
 def merge_linear_with_causal_model(model: ConstraintBasedModel, graph: GenericGraph, gene_metab_prefix='GXM_',
                                    gene_drain_prefix='GXD_', gene_pool_prefix='GXP_', gene_poolrx_prefix='ExGXP_',
                                    interaction_prefix='SRI_', max_flux=1e4):
-
     genes_in_causal = {g for g in graph.nodes}
     genes_in_model = {g[len(gene_metab_prefix):] for g in model.metabolite_names if g.startswith(gene_metab_prefix)}
     genes_to_add = genes_in_causal - genes_in_model
     total_genes = genes_in_causal | genes_in_model
 
     # remove all existing drains
-    model.remove_reactions([gene_drain_prefix+g for g in genes_in_model])
+    model.remove_reactions([gene_drain_prefix + g for g in genes_in_model])
 
     # add missing gene metabolites
     model.add_metabolites(
@@ -194,10 +201,12 @@ def merge_linear_with_causal_model(model: ConstraintBasedModel, graph: GenericGr
         [gene_pool_prefix + x for x in total_genes])
 
     # add reaction producing the gene metabolite and consuming its associated gene supply
-    gxd_names, gxd_args = zip(*((gene_drain_prefix+g, {gene_metab_prefix+g: 1, gene_pool_prefix+g:-1}) for g in total_genes))
-    gxp_names, gxp_args = zip(*((gene_poolrx_prefix+g, {gene_pool_prefix+g:1}) for g in total_genes))
+    gxd_names, gxd_args = zip(
+        *((gene_drain_prefix + g, {gene_metab_prefix + g: 1, gene_pool_prefix + g: -1}) for g in total_genes))
+    gxp_names, gxp_args = zip(*((gene_poolrx_prefix + g, {gene_pool_prefix + g: 1}) for g in total_genes))
 
-    model.add_reactions(args=gxd_args+gxp_args, bounds=[(0, max_flux)]*(len(gxd_args)+len(gxp_args)), names=gxd_names+gxp_names)
+    model.add_reactions(args=gxd_args + gxp_args, bounds=[(0, max_flux)] * (len(gxd_args) + len(gxp_args)),
+                        names=gxd_names + gxp_names)
 
     # get all of the graph's edges and store the source, target and associated weight
     graph_edges = graph.edges
@@ -213,20 +222,24 @@ def merge_linear_with_causal_model(model: ConstraintBasedModel, graph: GenericGr
             edge_arg[gene_pool_prefix + tar] = 1
         else:
             edge_arg[gene_pool_prefix + tar] = -1
-        sri_items.append([interaction_prefix+edge_name, edge_arg])
+        sri_items.append([interaction_prefix + edge_name, edge_arg])
 
     sri_names, sri_args = zip(*sri_items)
-    model.add_reactions(args=sri_args, bounds=[(0, max_flux)]*len(sri_args), names=sri_names)
+    model.add_reactions(args=sri_args, bounds=[(0, max_flux)] * len(sri_args), names=sri_names)
 
     return edge_dict
 
+
 class IntegratedGPRModel(ConstraintBasedModel):
     def __init__(self, model: ConstraintBasedModel, solver=None, gpr_rx_bound=1e4, optimizer=False, **kwargs):
+        integrated_irrev_model, cb_model_rev_map, mt_to_add, rx_to_gpr, metabolic_fluxes, metabolites = \
+            get_integrated_gpr_model(model, gpr_rx_bound, **kwargs)
+        print('Integrated GPR model', integrated_irrev_model.get_stoichiometric_matrix().shape)
 
-        integrated_irrev_model, cb_model_rev_map, mt_to_add = get_integrated_gpr_model(model, gpr_rx_bound, **kwargs)
-        print('Integrated GPR model',integrated_irrev_model.get_stoichiometric_matrix().shape)
-        self.metabolic_fluxes = tuple(integrated_irrev_model.reaction_names)
+        self.metabolic_fluxes = tuple(metabolic_fluxes)
         self.metabolic_rev_map = cb_model_rev_map
+        self.gpr_mapping = rx_to_gpr
+        self.metabolic_compound_names = tuple(metabolites)
 
         S = integrated_irrev_model.get_stoichiometric_matrix()
         bounds = integrated_irrev_model.bounds
@@ -249,13 +262,12 @@ class IntegratedGPRModel(ConstraintBasedModel):
 
 class IntegratedGPRCausalModel(IntegratedGPRModel):
     def __init__(self, model: ConstraintBasedModel, graph: GenericGraph, solver=None, gpr_rx_bound=1e4,
-                 gene_metab_prefix='GXM_',gene_drain_prefix='GXD_', gene_pool_prefix='GXP_',
+                 gene_metab_prefix='GXM_', gene_drain_prefix='GXD_', gene_pool_prefix='GXP_',
                  gene_poolrx_prefix='ExGXP_', interaction_prefix='SRI_', **kwargs):
-
         super().__init__(model, solver, gpr_rx_bound, **kwargs)
 
         self.__edges = merge_linear_with_causal_model(self, graph, gene_metab_prefix, gene_drain_prefix,
-                                                     gene_pool_prefix, gene_poolrx_prefix, interaction_prefix)
+                                                      gene_pool_prefix, gene_poolrx_prefix, interaction_prefix)
 
     @property
     def causal_interaction_edges(self):
